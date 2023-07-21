5d81caf7f5b72a869180179f128d95899bb30afd
Mohammed-az <mohammed.azaiza@mail.huji.ac.il>
2023-07-22 00:22:21 +0300

    environment mapping for dynamic reflections

--- source/apps/TestApp/src/TestApp.cpp
+++ source/apps/TestApp/src/TestApp.cpp
@@ -50,49 +50,6 @@ uint loadTexture(const std::string& filePath) {
     return textureID;
 }
 
-uint loadCubemapTexture(std::vector<std::string>& filenames) {
-    //Create a new cubemap texture
-    uint textureID;
-    glGenTextures(1, &textureID);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
-
-    //Cubemap textures should not be upside-down
-    stbi_set_flip_vertically_on_load(0);
-
-    for (int i = 0; i < 6; i++) {
-        //Load the image
-        int width, height, nrComponents;
-        unsigned char* data = stbi_load(filenames[i].c_str(), &width, &height, &nrComponents, 0);
-        if (data) {
-            GLenum format = 0;
-            if (nrComponents == 1)
-                format = GL_RED;
-            else if (nrComponents == 3)
-                format = GL_RGB;
-            else if (nrComponents == 4)
-                format = GL_RGBA;
-
-            //Create one side of cubemap texture from image
-            GLenum target = GL_TEXTURE_CUBE_MAP_POSITIVE_X + i;
-            glTexImage2D(target, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-
-            stbi_image_free(data);
-        } else {
-            LENNY_LOG_WARNING("Failed to load texture from path `%s`", filenames[i].c_str());
-        }
-
-        //Set texture parameters
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    }
-
-    glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
-    return textureID;
-}
-
 TestApp::TestApp() : gui::Application("TestApp") {
     //Setup scene
     const auto [width, height] = getCurrentWindowSize();
@@ -103,22 +60,24 @@ TestApp::TestApp() : gui::Application("TestApp") {
     };
     scenes.back()->f_fileDropCallback = [&](int count, const char** fileNames) -> void { fileDropCallback(count, fileNames); };
 
-    //Load environment textures
-    loadSkybox();
-}
-
-void TestApp::loadSkybox() {
     //Skybox textures
     std::vector<std::string> filenames = {
         //Use ordering of cube sides according to cubemap constants in glad.h
-        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/right.png",   // GL_TEXTURE_CUBE_MAP_POSITIVE_X
-        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/left.png",    // GL_TEXTURE_CUBE_MAP_NEGATIVE_X
-        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/top.png",     // GL_TEXTURE_CUBE_MAP_POSITIVE_Y
-        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/bottom.png",  // GL_TEXTURE_CUBE_MAP_NEGATIVE_Y
-        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/front.png",   // GL_TEXTURE_CUBE_MAP_POSITIVE_Z
-        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/back.png"     // GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
+        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/right.png",   //GL_TEXTURE_CUBE_MAP_POSITIVE_X
+        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/left.png",    //GL_TEXTURE_CUBE_MAP_NEGATIVE_X
+        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/top.png",     //GL_TEXTURE_CUBE_MAP_POSITIVE_Y
+        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/bottom.png",  //GL_TEXTURE_CUBE_MAP_NEGATIVE_Y
+        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/front.png",   //GL_TEXTURE_CUBE_MAP_POSITIVE_Z
+        LENNY_GUI_TESTAPP_FOLDER "/config/envmap/back.png"     //GL_TEXTURE_CUBE_MAP_NEGATIVE_Z
     };
 
+    //Create the skybox model and cubemaps
+    loadSkybox(filenames);
+    staticCubemap.load(filenames);
+    dynamicCubemap.create();
+}
+
+void TestApp::loadSkybox(std::vector<std::string>& filenames) {
     //Vertices for a square side of the skybox, each containing position, normal, texCoords
     std::vector<gui::Model::Mesh::Vertex> vertices = {
         {glm::vec3(-1, -1, 1), glm::vec3(0, 0, -1), glm::vec2(0, 0)},
@@ -139,16 +98,13 @@ void TestApp::loadSkybox() {
         //Append a new mesh to the skybox
         skybox.emplace_back(vertices, indices, material);
     }
-
-    //Create cubemap texture
-    texture_cubemap = loadCubemapTexture(filenames);
 }
 
 void TestApp::drawSkybox() const {
     //Skybox size
     glm::mat4 scale = glm::scale(glm::mat4(1), glm::vec3(50));
 
-    gui::Shaders::activeShader->activate();
+    gui::Shaders::activeShader->setBool("isSkybox", true);
     gui::Shaders::activeShader->setFloat("objectAlpha", 1.0f);
 
     //Draw right side
@@ -180,36 +136,88 @@ void TestApp::drawSkybox() const {
     transform = glm::rotate(scale, glm::radians(180.0f), glm::vec3(0, 1, 0));
     gui::Shaders::activeShader->setMat4("modelPose", transform);
     skybox[5].draw(std::nullopt);
+
+    gui::Shaders::activeShader->setBool("isSkybox", false);
 }
 
-void TestApp::drawScene() const {
-    //Draw skybox without environment mapping
+void TestApp::updateDynamicCubemap(int modelIndex) {
+    //Bind the cubemap framebuffer
+    dynamicCubemap.startUpdating();
+
+    //Use the model's position as a view point
+    glm::vec3 position = gui::utils::toGLM(models[modelIndex].position);
+    gui::Shaders::activeShader->setVec3("cameraPosition", position);
+
+    //Use the 90-degree field of view
+    glm::mat4 projection = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 1000.0f);
+    gui::Shaders::activeShader->setMat4("cameraProjection", projection);
+
+    //Render to each side of the cubemap
+    for (int side = 0; side < 6; side++) {
+        //Select the cubemap side
+        glm::mat4 view = dynamicCubemap.selectSide(GL_TEXTURE_CUBE_MAP_POSITIVE_X + side, position);
+        gui::Shaders::activeShader->setMat4("cameraView", view);
+
+        //Draw the skybox
+        drawSkybox();
+
+        //Draw the models
+        std::optional<Eigen::Vector3d> modelColor = std::nullopt;
+        if (!showMaterials)
+            modelColor = rendererColor.segment(0, 3);
+        for (int i = 0; i < (int)models.size(); i++) {
+            //Skip itself to avoid problems with self-reflections
+            if (i == modelIndex)
+                continue;
+
+            //Don't draw the reference sphere if unchecked
+            if (!showReferenceSphere && models[i].mesh.filePath.ends_with("sphere.obj"))
+                continue;
+
+            //Draw the model
+            models[i].mesh.draw(models[i].position, models[i].orientation, models[i].scale, modelColor, rendererColor[3]);
+        }
+    }
+
+    //Unbind the cubemap framebuffer
+    dynamicCubemap.stopUpdating();
+
+    //Restore the uniform values
+    gui::Shaders::update(scenes.back()->camera, scenes.back()->light);
+}
+
+void TestApp::drawScene() {
+    //Enable environment mapping if checked
     gui::Shaders::activeShader->activate();
     gui::Shaders::activeShader->setBool("enableEnvironmentMapping", enableEnvironmentMapping);
     gui::Shaders::activeShader->setInt("environmentMappingType", environmentMappingType);
-    gui::Shaders::activeShader->setBool("isSkybox", true);
+
+    //Activate the cubemap texture unit
+    gui::Shaders::activeShader->setInt("texture_cubemap", 1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, enableDynamicReflections ? dynamicCubemap.texture : staticCubemap.texture);
+
+    //Draw the skybox
     drawSkybox();
-    gui::Shaders::activeShader->setBool("isSkybox", false);
 
-    //Activate cubemap texture unit
-    if (texture_cubemap.has_value()) {
-        glActiveTexture(GL_TEXTURE1);
-        glUniform1i(glGetUniformLocation(gui::Shaders::activeShader->getID(), "texture_cubemap"), 1);
-        glBindTexture(GL_TEXTURE_CUBE_MAP, texture_cubemap.value());
-    }
-
-    //Draw models
+    //Draw the models
     std::optional<Eigen::Vector3d> modelColor = std::nullopt;
     if (!showMaterials)
         modelColor = rendererColor.segment(0, 3);
-    for (const AppModel& model : models)
-        model.mesh.draw(model.position, model.orientation, model.scale, modelColor, rendererColor[3]);
+    for (int i = 0; i < (int)models.size(); i++) {
+        //Don't draw the reference sphere if unchecked
+        if (!showReferenceSphere && models[i].mesh.filePath.ends_with("sphere.obj"))
+            continue;
 
-    //Draw a reference sphere to better see the reflections
-    if (showReferenceSphere)
-        gui::Renderer::I->drawSphere(Eigen::Vector3d(-2, 0, 1), 1.0, Eigen::Vector4d(0.6, 0.6, 0.6, 1));
+        //Update the cubemap texture for this model if checked
+        if (enableDynamicReflections)
+            updateDynamicCubemap(i);
 
-    //Disable environment mapping for ground
+        //Draw the model
+        models[i].mesh.draw(models[i].position, models[i].orientation, models[i].scale, modelColor, rendererColor[3]);
+    }
+
+    //Disable environment mapping for ground (rendered by LennyGraphics)
     gui::Shaders::activeShader->setBool("enableEnvironmentMapping", false);
 }
 
@@ -228,6 +236,17 @@ void TestApp::drawGui() {
     ImGui::Checkbox("Enable environment mapping", &enableEnvironmentMapping);
 
     if (enableEnvironmentMapping) {
+        ImGui::Indent();
+
+        //Radio buttons for static/dynamic cubemap type
+        ImGui::Text("Cubemap type");
+        ImGui::Indent();
+        if (ImGui::RadioButton("Static", !enableDynamicReflections))
+            enableDynamicReflections = false;
+        if (ImGui::RadioButton("Dynamic", enableDynamicReflections))
+            enableDynamicReflections = true;
+        ImGui::Unindent();
+
         //Radio buttons for environment mapping type
         ImGui::Text("Mapping type");
         ImGui::Indent();
@@ -235,6 +254,8 @@ void TestApp::drawGui() {
         ImGui::RadioButton("Multiply", &environmentMappingType, 1);
         ImGui::RadioButton("Average", &environmentMappingType, 2);
         ImGui::RadioButton("Mix", &environmentMappingType, 3);
+        ImGui::Unindent();
+
         ImGui::Unindent();
     }
 
